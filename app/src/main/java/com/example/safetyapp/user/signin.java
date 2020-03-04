2fda594f1c20fdbbe2ef0f5781c48f205907b838
hiteshmdarji <43123882+hiteshmdarji@users.noreply.github.com>
2020-03-04 11:33:52 +0530

    forget module

--- app/src/main/java/com/example/safetyapp/user/signin.java
+++ app/src/main/java/com/example/safetyapp/user/signin.java
@@ -6,9 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.safetyapp.MainActivity;
 import com.example.safetyapp.R;
+
+import org.w3c.dom.Text;
 
 public class signin extends AppCompatActivity {
 
@@ -17,11 +20,20 @@ public class signin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
 
-        Button signin=(Button)findViewById(R.id.signin);
+        final Button signin=(Button)findViewById(R.id.signin);
         signin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i=new Intent(signin.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        TextView forgetpasstext=(TextView)findViewById(R.id.forgetpasstext);
+        forgetpasstext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(signin.this,forget_password.class);
                 startActivity(i);
             }
         });
