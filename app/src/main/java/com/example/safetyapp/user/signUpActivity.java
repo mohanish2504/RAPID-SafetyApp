4858d6d494cec5b6f518d97b522798eb3d6d4259
Mohan <mohanishp1@gmail.com>
2020-05-06 15:59:37 +0530

    signup and LOgin

--- app/src/main/java/com/example/safetyapp/user/signUpActivity.java
+++ app/src/main/java/com/example/safetyapp/user/signUpActivity.java
@@ -3,10 +3,12 @@ package com.example.safetyapp.user;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
 
@@ -17,18 +19,26 @@ import java.util.Calendar;
 public class signUpActivity extends AppCompatActivity {
     TextView dob;
     DatePickerDialog.OnDateSetListener setListener;
-
+    Button Submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
         dob = findViewById(R.id.dob);
-
+        Submit =  (Button) findViewById(R.id.SubmitButton);
         Calendar calendar = Calendar.getInstance();
         final int year = calendar.get(Calendar.YEAR);
         final int month = calendar.get(Calendar.MONTH);
         final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        Submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Intent intent = new Intent(getApplicationContext(),phoneno.class);
+               startActivity(intent);
+            }
+        });
 
         dob.setOnClickListener(new View.OnClickListener() {
             @Override
