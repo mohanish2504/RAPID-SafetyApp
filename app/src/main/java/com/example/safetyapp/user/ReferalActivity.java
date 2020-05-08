abfeccfd88b03bf7ed6cb5d265fbb1cd3a16fd9b
Mohan <mohanishp1@gmail.com>
2020-05-08 11:03:23 +0530

    referal listview

--- app/src/main/java/com/example/safetyapp/user/ReferalActivity.java
+++ app/src/main/java/com/example/safetyapp/user/ReferalActivity.java
@@ -1,24 +1,37 @@
 package com.example.safetyapp.user;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.example.safetyapp.R;
 
+import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
+
+import static android.provider.CalendarContract.CalendarCache.URI;
+import static com.example.safetyapp.user.EmergencyContact.RequestPermissionCode;
 
 public class ReferalActivity extends AppCompatActivity {
 
@@ -26,19 +39,33 @@ public class ReferalActivity extends AppCompatActivity {
     String Names[] = {"Name 1","Name 2"};
     String Codes[] = {"xyz","xyz"};
     private String TAG= ReferalActivity.class.getSimpleName();
+    ArrayList<EmergencyContact> emergencyContacts;
+    MyAdapter myAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_referal);
 
+        EnableRuntimePermission();
+
         listView = (ListView) findViewById(R.id.list);
-        EmergencyContact[] emergencyContacts = new EmergencyContact[2];
+        emergencyContacts=new ArrayList<EmergencyContact>();
 
-        emergencyContacts[0] = new EmergencyContact();
-        emergencyContacts[1] = new EmergencyContact();
-
-        MyAdapter myAdapter = new MyAdapter(this,R.layout.activity_emergency_contact,emergencyContacts);
+        myAdapter = new MyAdapter(this,R.layout.activity_emergency_contact,emergencyContacts);
         listView.setAdapter(myAdapter);
+
+
+        Button selectContact = (Button) findViewById(R.id.selectContact);
+        selectContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                startActivityForResult(intent, 7);
+                //myAdapter.notifyDataSetChanged();
+                //Log.d(TAG,Integer.toString(emergencyContacts.size()));
+            }
+        });
+
 
         TextView txtDetails = (TextView) findViewById(R.id.txtDetails);
         txtDetails.setText("Note: Please send link to contacts and install the app in your \n" +
@@ -47,12 +74,22 @@ public class ReferalActivity extends AppCompatActivity {
 
     }
 
+    public void EnableRuntimePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CONTACTS)) {
+            Toast.makeText(getApplicationContext(), "CONTACTS permission allows us to Access CONTACTS app", Toast.LENGTH_LONG).show();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.READ_CONTACTS}, RequestPermissionCode);
+
+        }
+    }
+
     class MyAdapter extends ArrayAdapter<EmergencyContact>{
 
-        EmergencyContact[] emergencyContacts;
+        ArrayList<EmergencyContact> emergencyContacts;
         Context context;
         private String TAG= MyAdapter.class.getSimpleName();
-        public MyAdapter(@NonNull Context context, int resource,EmergencyContact[] emergencyContacts) {
+        public MyAdapter(@NonNull Context context, int resource,ArrayList<EmergencyContact> emergencyContacts) {
             super(context, resource);
             //Log.d(TAG,"new adapter");
             this.context = context;
@@ -63,7 +100,7 @@ public class ReferalActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            return emergencyContacts.length;
+            return emergencyContacts.size();
         }
 
         @NonNull
@@ -79,10 +116,10 @@ public class ReferalActivity extends AppCompatActivity {
             TextView textView_name = (TextView) convertView.findViewById(R.id.contacts_textview_name);
             EditText editText_code = (EditText) convertView.findViewById(R.id.contacts_textview_number);
 
-            Log.d(TAG,this.emergencyContacts[position].getName());
+            Log.d(TAG,this.emergencyContacts.get(position).getName());
 
-            textView_name.setText(emergencyContacts[position].getName());
-            editText_code.setHint(emergencyContacts[position].getReferal());
+            textView_name.setText(this.emergencyContacts.get(position).getName());
+            editText_code.setHint(this.emergencyContacts.get(position).getReferal());
 
             //Log.d("Howdi","here");
 
@@ -94,10 +131,20 @@ public class ReferalActivity extends AppCompatActivity {
     class EmergencyContact{
         String name;
         String referal;
+        String number;
 
-        public EmergencyContact(String name, String referal) {
+        public String getNumber() {
+            return number;
+        }
+
+        public void setNumber(String number) {
+            this.number = number;
+        }
+
+        public EmergencyContact(String name, String referal,String number) {
             this.name = name;
             this.referal = referal;
+            this.number = number;
         }
 
         public EmergencyContact() {
@@ -122,4 +169,42 @@ public class ReferalActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG,Integer.toString(requestCode));
+        if(requestCode==7){
+            //if(requestCode==RESULT_OK){
+                //Log.d()
+                Log.d(TAG,"here");
+                EmergencyContact emergencyContact_temp;
+                Uri contactData = data.getData();
+                String number = "";
+                String name = "";
+                Cursor cursor = getContentResolver().query(contactData, null, null, null, null);
+                cursor.moveToFirst();
+                String hasPhone = cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.Contacts.HAS_PHONE_NUMBER));
+                String contactId = cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.Contacts._ID));
+                if (hasPhone.equals("1")) {
+                    Cursor phones = getContentResolver().query
+                            (ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+                                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID
+                                            + " = " + contactId, null, null);
+                    while (phones.moveToNext()) {
+                        number = phones.getString(phones.getColumnIndex
+                                (ContactsContract.CommonDataKinds.Phone.NUMBER)).replaceAll("[-() ]", "");
+                        name = phones.getString(phones.getColumnIndex
+                                (ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)).replaceAll("[-() ]", "");
+                        emergencyContact_temp = new EmergencyContact(name,"xyz",number);
+                        emergencyContacts.add(emergencyContact_temp);
+                        myAdapter.notifyDataSetChanged();
+                    }
+                    phones.close();
+                } else {
+                    Toast.makeText(getApplicationContext(), "This contact has no phone number", Toast.LENGTH_LONG).show();
+                }
+                cursor.close();
+            //}
+        }
+    }
 }
