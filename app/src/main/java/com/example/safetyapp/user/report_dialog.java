452e35572b70c7122a8324ad5a8ce116d045fe7c
hiteshmdarji <43123882+hiteshmdarji@users.noreply.github.com>
2020-05-20 17:45:52 +0530

    Report Done

--- app/src/main/java/com/example/safetyapp/user/report_dialog.java
+++ app/src/main/java/com/example/safetyapp/user/report_dialog.java
@@ -2,10 +2,9 @@ package com.example.safetyapp.user;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,28 +12,62 @@ import androidx.appcompat.app.AppCompatDialogFragment;
 
 import com.example.safetyapp.R;
 
+import java.util.ArrayList;
+
 public class report_dialog extends AppCompatDialogFragment {
+
+    public interface onMultiChoiceListener{
+        void onPositiveButtonClicked(String[] list, ArrayList<String> selectedItemList);
+        void onNegativeButtonClicked();
+    }
+
+    onMultiChoiceListener mListener;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        try {
+            mListener= (onMultiChoiceListener) context;
+        } catch (Exception e) {
+            //throw new ClassCastException(getActivity().toString()+"onMultiChoiceListener must implemented");
+        }
+    }
 
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
-        LayoutInflater inflater = getActivity().getLayoutInflater();
-        View view = inflater.inflate(R.layout.report_dialog, null);
+        final ArrayList<String> selectedItemList = new ArrayList<>();
 
-        builder.setView(view)
-                .setTitle("Report User")
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.MyDialogTheme);
+
+        final String[] list = getActivity().getResources().getStringArray(R.array.report_reason);
+
+
+        builder.setTitle("Report User")
+                .setMultiChoiceItems(list, null, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+
+                        if(isChecked){
+                            selectedItemList.add(list[which]);
+                        }
+                        else{
+                            selectedItemList.remove(list[which]);
+                        }
+
+                    }
+                })
                 .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-
+                        mListener.onNegativeButtonClicked();
                     }
                 })
                 .setPositiveButton("Submit", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-
+                        mListener.onPositiveButtonClicked(list, selectedItemList);
                     }
                 });
         return  builder.create();
